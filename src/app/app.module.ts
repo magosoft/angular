307906b50c5706883ca2d1ee28e965cecaf4d787
Guido Caceres Pinto <dcaceres@grupo-lafuente.com>
2021-08-16 18:56:18 -0400

    Guardando servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatDialogModule } from '@angular/material/dialog';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PipelineComponent } from './components/pipeline/pipeline.component';
+import { EditviewComponent } from './components/pipeline/editview/editview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipelineComponent,
+    EditviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
